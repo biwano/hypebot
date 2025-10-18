2c4e296c5375ade5a4bd95289f23dffb0870486c
biwano <biwano@gmail.com>
2025-10-18 22:45:34 +0200

    To express!

--- plugins/supabase.client.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtPlugin(() => {
-  // The Supabase client is automatically available via @nuxtjs/supabase
-  // This plugin can be used for additional Supabase configuration if needed
-})
-
